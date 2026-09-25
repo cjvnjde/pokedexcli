f8dc82a8343c399f384c79ca2e6f1ac75b7d8fe8
Vitalij Nykyforenko <vitalij.nykyforenko@pm.me>
2026-09-25 13:29:09 +0300

    CH1 L4

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+
+		if ok {
+			input := scanner.Text()
+			data := cleanInput(input)
+
+			if len(data) > 0 {
+				firstWord := data[0]
+				fmt.Printf("Your command was: %s\n", firstWord)
+			}
+		}
+	}
 }
