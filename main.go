317254e7c62e04c3d5de1715f41f576b527015f7
Vitalij Nykyforenko <vitalij.nykyforenko@pm.me>
2026-09-25 13:49:42 +0300

    CH1 L5

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -18,9 +19,54 @@ func main() {
 			data := cleanInput(input)
 
 			if len(data) > 0 {
-				firstWord := data[0]
-				fmt.Printf("Your command was: %s\n", firstWord)
+				command := data[0]
+
+				if cm, ok := commands[command]; ok {
+					cm.callback()
+				} else {
+					fmt.Println("Unknown command")
+				}
 			}
 		}
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+
+	return nil
+}
+
+func helpCommand() error {
+	commands := getCommands()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+
+	for code, command := range commands {
+		fmt.Printf("%s: %s\n", code, command.description)
+	}
+
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    helpCommand,
+		},
 	}
 }
